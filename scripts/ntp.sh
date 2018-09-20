3b6b834663559bc00c8f48e5f4ee58a9b0189b95
Sohaib Azed <sohaibazed@gmail.com>
2018-09-20 11:17:28 -0400

    updated root password for VMs

--- scripts/ntp.sh
+++ scripts/ntp.sh
@@ -9,7 +9,7 @@ sudo sed '/^#PermitRootLogin/a PermitRootLogin yes' -i /etc/ssh/sshd_config
 sudo sed -i '/^#ListenAddress 0.0.0.0/s/^#//' -i /etc/ssh/sshd_config
 sudo sed '/^#PasswordAuthentication/a PasswordAuthentication yes' -i /etc/ssh/sshd_config
 sudo systemctl restart sshd
-echo "root:Juniper123" | sudo chpasswd
+echo "root:c0ntrail123" | sudo chpasswd
 sudo iptables -F
 
 # Install packages
